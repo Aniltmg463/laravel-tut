bc1e2c65f3671f745a00e7eaf08972a914177381
https://github.com/Aniltmg463/Tech-hub.git <aniltamang462@gmail.com>
2025-05-05 09:22:30 +0545

    ep7 : nae Routes and routing group

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<h1>About<h1>
+
+    <a href="{{route('home') }}">Home2 Page </a> <br> <br>
+    <a href="{{route('myabout') }}">About Page </a>
--- routes/web.php
+++ routes/web.php
@@ -4,72 +4,53 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+//second topic
+/* Route::get('/about', function () {
+    return view('about');
+})->name('myabout');
+ */
+/* Route::get('/postssss', function () {
+    return view('post');
+})->name('mypost'); */
+/* 
+Route::get('post/posts', function () {
+    return view('post');
+})->name('mypost');
+
+Route::get('/test', function () {
+    return view('about');
+})->name('myabout'); */
+
+// Route::redirect('/about', '/test');
+// Route::redirect('/about', '/test', 301); //301  permanent redirect most used. listed below
+/* 
+| Status Code | Meaning                                | Usage in Laravel                                          |
+| ----------- | -------------------------------------- | --------------------------------------------------------- |
+| `302`       | **Found** (default temporary redirect) | `return redirect('home');`                                |
+| `301`       | **Moved Permanently**                  | `return redirect('home', 301);`                           |
+| `303`       | **See Other** (redirect after POST)    | `return redirect()->route('login')->with('status', 303);` |
+| `307`       | **Temporary Redirect**                 | Rarely used manually                                      |
+| `308`       | **Permanent Redirect**                 | For strict clients needing method preservation            |
+ */
+
+
+//third topic: ROutes group
+Route::group(['prefix' => 'page'], function () {
+    Route::get('/about', function () {
+        return '<h1>About Page</h1>';
+    });
+
+    Route::get('/gallery', function () {
+        return '<h1>Gallery Page</h1>';
+    });
+
+    Route::get('/post/firstpost', function () {
+        return '<h1>First Post Page</h1>';
+    });
 });
 
-
-//only for routing topic day1
-// Route::get('/post', function () {
-//     return view('post');
-//     //return "<h1>Post Page</h1>";
-// });
-
-// //alternative method to write route
-// // Route :: view('post','/post');
-
-// //sub routing eg
-// Route::get('/post/firstpost', function () {
-//     return view('firstpost');
-//     //return "<h1>Post Page</h1>";
-// });
-
-//day2 for routing parameter
-// Route::get('/post/{id?}', function (string $id=null) {
-//     // return view('firstpost');
-//     return "<h1>Post ID:" . $id . "</h1>";
-// });
-
-// Route::get('/post/{id?}', function (string $id=null) {
-//     if($id){
-//         return "<h1>Post ID:" . $id . "</h1>";
-//     }else{
-//         return "<h1>No ID found</h1>";
-//     }
-// });
-
-//multuple roting parameter
-// Route::get('/post/{id?}/comment/{comment?}', function (string $id=null, string $comment=null) {
-//     if($id){
-//         return "<h1>Post ID: " . $id . "</h1><h2>Comment ID: " . $comment . "</h2>";
-//     }else{
-//         return "<h1>No ID found</h1>";
-//     }
-// });
-
-// //Route constraint day2
-// Route::get('/post/{id}', function (string $id) {
-//     if($id){
-//         return "<h1>Post ID: " . $id . "</h1>";
-//     }else{
-//         return "<h1>No ID found</h1>";
-//     }
-// // })->whereNumber('id'); //numeica value
-// // })->whereAlpha('id'); //alpha value
-// // })->whereAlphaNumeric('id'); //alpha n num value
-// // })->whereIn('id',['movie','song','painting']);
-// // })->where('id',[0-9]+''); //regular expression fro numeric value
-// })->where('id','[a-zA-Z]+'); //regular expression fro alphabate value
-
-// /* multiple route constraint */
-Route::get('/post/{id}/comment/{commentid}', function (string $id, string $comment) {
-    if($id){
-        return "<h1>Post ID: " . $id . "& Comment: ". $comment . "</h1>";
-    }else{
-        return "<h1>No ID found</h1>";
-    }
-// })->whereNumber('id'); //numeica value
-// })->whereAlpha('id'); //alpha value
-// })->whereAlphaNumeric('id'); //alpha n num value
-// })->whereIn('id',['movie','song','painting']);
-// })->where('id',[0-9]+''); //regular expression fro numeric value
-})->where('id','[0-9]+')->whereAlpha('commentid'); //regular expression fro alphabate v
-
+Route::fallback(function () {
+    return '<h1>404 Page Not Found</h1>';
+});
