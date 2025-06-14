b2e900532eafd75f2b8769c19a16b01629a27daf
Anil463 <aniltamang462@gmail.com>
2025-06-14 20:50:50 +0545

    Laravel Resource Controller
    add PostController and PhotoController

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\UserController;
 // use App\Http\Controllers\CommentContoller;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 //Normal Routes:
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome', ['name' => 'Laravel 12', 'word' => 'Framework']);
+});
 
 
 //Single Resource Controller Route:
@@ -16,6 +18,12 @@ Route::resource('users', UserController::class); //use for  all method
 // Route::resource('users', UserController::class)->only(['create', 'update', 'show']);
 // Route::resource('users', UserController::class)->names(['create' => 'users.build', 'show' => 'users.view']);
 
+//Multipel Resource Controller
+Route::resources([
+    'photos' => PhotoController::class,
+    'posts' => PostController::class,
+]);
+
 //Nested Resources  Controller Route:
 // Route::resource('users.comments', CommentController::class);
-Route::resource('users.comments', CommentController::class)->shallow(); /* mUse the full nested route only for routes that need the parent (user) — like index, create, store. For routes that act on a single comment, remove the user part from the URL. */
+Route::resource('users.comments', CommentController::class)->shallow(); /* Use the full nested route only for routes that need the parent (user) — like index, create, store. For routes that act on a single comment, remove the user part from the URL. */
