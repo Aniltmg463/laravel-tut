11040603bf7bb673e9cc4bff7b2b24611675f932
Anil463 <aniltamang462@gmail.com>
2025-06-14 16:49:38 +0545

    ep30 Laravel Resource controller

--- resources/views/pages/sidebar.blade.php
+++
@@ -1,8 +0,0 @@
-<aside>
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li>  <a href="/about">About</a></li>
-      <li>  <a href="/post">Post Services</a></li>
-      <li>  <a href="/contact">Contact</a></li>
-    </ul>
-  </aside>
