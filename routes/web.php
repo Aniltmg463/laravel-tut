289ef82380deb93ca22bfd9a95cc6e1229be30ff
Anil463 <aniltamang462@gmail.com>
2025-06-14 16:51:06 +0545

    ep30 Laravel Resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,21 @@
 <?php
 
+use App\Http\Controllers\UserController;
+// use App\Http\Controllers\CommentContoller;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/homepage', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('welcome2');
-});
+//Normal Routes:
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::get('/about', function () {
-    return view('about');
-});
+//Single Resource Controller Route:
+Route::resource('users', UserController::class); //use for  all method
+// Route::resource('users', UserController::class)->only(['create', 'update', 'show']);
+// Route::resource('users', UserController::class)->names(['create' => 'users.build', 'show' => 'users.view']);
 
-Route::get('/post', function () {
-    return view('post');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+//Nested Resources  Controller Route:
+// Route::resource('users.comments', CommentController::class);
+Route::resource('users.comments', CommentController::class)->shallow(); /* mUse the full nested route only for routes that need the parent (user) — like index, create, store. For routes that act on a single comment, remove the user part from the URL. */
